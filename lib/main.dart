f845222ea439b9f6112534ba5f44c643dcc1e4d5
harshchovatiya <harshchovatiya5312@gmail.com>
2020-05-13 11:53:23 +0530

    -Initial commit. -Implement qrcode scanner.

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String barcode = "";
+  Future scanCode() async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+      setState(() => this.barcode = barcode);
+    } catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          this.barcode = 'The user did not grant the camera permission!';
+        });
+      } else {
+        setState(() => this.barcode = 'Unknown error: $e');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Barcode Scan'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(barcode),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: scanCode,
+        tooltip: 'Scan',
+        child: Icon(Icons.scanner),
+      ),
+    );
+  }
+}
